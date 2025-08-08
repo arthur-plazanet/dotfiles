f54737354ccb740e4a3d626153a8d3535bb7a464
Arthur Plazanet <plazanet.art@gmail.com>
2025-08-08 19:56:45 +0200

    Add telescope project to test

--- nvim/lua/plugins/live-server.lua
+++ nvim/lua/plugins/live-server.lua
@@ -5,6 +5,6 @@ return {
   config = true,
   keys = {
     { '<leader>ls', ':LiveServerStart<Enter>', desc = 'Start Live Server' },
-    { '<leader>lq', ':LiveServerStop<Enter>',  desc = 'Stop Live Server' },
+    { '<leader>lq', ':LiveServerStop<Enter>', desc = 'Stop Live Server' },
   },
 }
--- nvim/lua/plugins/telescope-project.lua
+++ nvim/lua/plugins/telescope-project.lua
@@ -1,3 +1,4 @@
+-- TODO: To test
 return {
   'nvim-telescope/telescope-project.nvim',
   enabled = false,
