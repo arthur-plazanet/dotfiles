161194cbfa8e32bcbcd323f8807f3690a845fabc
arthur-plazanet <72502545+arthur-plazanet@users.noreply.github.com>
2025-11-19 02:57:21 +0100

    Refactor autocompletion + README (#3)

---
+++ nvim/checklist.md
@@ -0,0 +1,9 @@
+- [ ] [blame.nvim](https://github.com/FabijanZulj/blame.nvim) - Neovim fugitive style git blame plugin
+- [ ] [Trouble.nvim](https://github.com/folke/trouble.nvim) - A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
+- [ ] [cmp integration for Vue](https://github.com/vuejs/language-tools/discussions/4495)
+- [ ] [mini.completion](https://github.com/echasnovski/mini.completion)
+- [ ] https://github.com/2KAbhishek/nerdy.nvim/
+      https://github.com/ya2s/nvim-nonicons
+      https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-icons.md
+      https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-files.md
+      https://cj.rs/telescope-repo-nvim/
---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,71 @@
+-- lua/plugins/cmp.lua
+local M = {
+  -- Autocompletion
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    -- Snippet engine
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+
+    -- Snippet collection (VS Code-style)
+    "rafamadriz/friendly-snippets",
+
+    -- You don't strictly need cmp-nvim-lsp here, we'll use it in lsp.lua
+    -- but you could also list it here if you prefer.
+    -- Adds LSP completion capabilities
+    'hrsh7th/cmp-nvim-lsp',
+  },
+}
+
+function M.config()
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
+
+  -- Load VS Code-style snippets (including friendly-snippets)
+  require("luasnip.loaders.from_vscode").lazy_load()
+
+  -- Your custom Lua snippets
+  require("luasnip.loaders.from_lua").lazy_load({
+    paths = "~/config/nvim/lua/config/snippets",
+  })
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        -- This makes LSP snippet items actually expand
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-Tab>"] = cmp.mapping.select_next_item(),
+      ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<CR>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }),
+      -- You can still leave <Tab> free for Copilot
+    }),
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" }, -- <- this is how friendly-snippets show up
+      { name = 'friendly-snippets' },
+      -- add more (buffer, path, etc.) if you want
+    },
+  })
+
+  -- Example: SQL-specific completion (vim-dadbod)
+  cmp.setup.filetype("sql", {
+    sources = {
+      { name = "vim-dadbod-completion" },
+      { name = "buffer" },
+
+    },
+  })
+end
+
+return M
